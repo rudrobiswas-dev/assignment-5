7bfb506000f052c2886d2fa9472e5485cd7888db
rudrobiswas-dev <rudrobiswas11023@gmail.com>
2026-03-08 04:04:21 +0600

    Work on navbar filtering search bar and button filtering and active

--- script.js
+++ script.js
@@ -13,23 +13,17 @@ loadIssues()
 // Display Issues
 
 function displayIssues(issues){
-
-container.innerHTML = ""
-
-document.getElementById("issueCount").innerText = issues.length
-
-issues.forEach(issue => {
-
-const div = document.createElement("div")
-
-div.className = "bg-white rounded-md shadow p-5"
-
-if(issue.status === "closed"){
-div.style.borderTop = "4px solid purple"
-}else{
-div.style.borderTop = "4px solid green"
-}
-
+    container.innerHTML = ""
+    document.getElementById("issueCount").innerText = issues.length
+    issues.forEach(issue => {
+    const div = document.createElement("div")
+    div.className = "bg-white rounded-md shadow p-5"
+    if(issue.status === "closed"){
+        div.style.borderTop = "4px solid purple"
+    }else{
+        div.style.borderTop = "4px solid green"
+    }
+    
 div.innerHTML = `
                     <div class="flex justify-end mb-5 pb-3">
                         <span class="text-sm bg-gray-300 px-4 py-1 rounded-full cursor-pointer">${issue.priority}</span>
@@ -52,8 +46,52 @@ div.innerHTML = `
                     ${issue.createdAt}
                     </div>
                 `
+                
 container.appendChild(div)
 
 })
 
+}
+
+// Filter Search text
+async function searchIssue(){
+const text = document.getElementById("searchInput").value
+const res = await fetch(
+`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`
+)
+const data = await res.json()
+displayIssues(data.data)
+}
+
+// Filter all || open || close || Active button
+// Active button function
+function setActive(btn){
+    const buttons = document.querySelectorAll(".tab-btn")
+    buttons.forEach(b=>{
+        b.classList.remove("btn-primary")
+    })
+    btn.classList.add("btn-primary")
+}
+//Filter All
+async function filterAll(btn){
+    setActive(btn)
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data = await res.json()
+displayIssues(data.data)
+}
+//Filter Open
+async function filterOpen(btn){
+    setActive(btn)
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data = await res.json()
+    const open = data.data.filter(issue => issue.status === "open")
+displayIssues(open)
+}
+//Filter Close
+async function filterClosed(btn){
+    setActive(btn)
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data = await res.json()
+    const closed = data.data.filter(issue => issue.status === "closed")
+displayIssues(closed)
 }
